1b5d703c5376520498782bc9e8fdc63ac9800b75
Pafnouti <corentinlemoine22@gmail.com>
2020-03-09 11:16:51 +0100

    Basic pwm driver implemented

---
+++ src/drivers/src/pwm_pololu_driver.cpp
@@ -0,0 +1,51 @@
+#include "ros/ros.h"
+#include "tf/tf.h"
+#include "std_msgs/Float64.h"
+#include <string>
+
+using namespace std;
+
+int baudrate;
+string portname;
+
+double steering, speed;
+
+void steeringCallback(const std_msgs::Float64::ConstPtr &msg)
+{
+    // (ROS_DEBUG, ROS_WARN, ROS_FATAL, ... )
+    steering = msg->data;
+    //ROS_INFO("I heard: [%f]", msg->angular.z);
+}
+
+void speedCallback(const std_msgs::Float64::ConstPtr &msg)
+{
+    // (ROS_DEBUG, ROS_WARN, ROS_FATAL, ... )
+    speed = msg->data;
+    //ROS_INFO("I heard: [%f]", msg->angular.z);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "CJC");
+    ros::NodeHandle n;
+
+    n.param<string>("port", portname, "/dev/ttyUSB1");
+    n.param<int>("baud", baudrate, 9600);
+
+    steering = 0.;
+    speed = 0.;
+
+    ros::Subscriber consigne_sub = n.subscribe("steeringSetpoint", 10, steeringCallback);
+    ros::Subscriber consigne_sub = n.subscribe("speedSetpoint", 10, speedCallback);
+
+    ros::Rate loop_rate(25);
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        
+        //TODO : send command
+
+        loop_rate.sleep();
+    }
+    return 0;
+}
