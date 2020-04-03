f118b91f1b18aa54483c350b30f62fd3a6fc36ee
Pafnouti <corentinlemoine22@gmail.com>
2020-04-03 17:19:09 +0200

    pwm driver node finished

--- ROS_package/src/drivers/pwm_driver/src/pwm_driver_node.py
+++ ROS_package/src/drivers/pwm_driver/src/pwm_driver_node.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python
 import rospy
+import maestro
 from std_msgs.msg import Float32
 
+"""
+Pololu's Maestro Windows installer sets up the Maestro Control Center, used to configure, test and program the controller. 
+Be sure the Maestro is configured for "USB Dual Port" serial mode, which is not the default.
+https://github.com/FRC4564/Maestro/blob/master/README.md
+
+------------
+Channels
+0 : throttle
+1 : steering
+"""
+
 def callbackThrottle(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    """
+    Data between -100 and 100
+    """
+    # target has to be between 4000 and 8000
+
+    target = 10*(600 + data*2)
+
+    servo.setTarget(0,target)
 
 def callbackSteering(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    
+    """
+    Data between -100 and 100
+    """
+    # target has to be between 4000 and 8000
+
+    target = 10*(600 + data*2)
+
+    servo.setTarget(1,target)
+
+
 def listener():
 
     rospy.init_node('pwm_driver', anonymous=True)
@@ -15,8 +42,12 @@ def listener():
     rospy.Subscriber("throttle_cmd", Float32, callbackThrottle)
     rospy.Subscriber("steering_cmd", Float32, callbackSteering)
 
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
+
 if __name__ == '__main__':
+    port = "/dev/ttyACM0"
+    servo = maestro.Controller(port)
+    servo.setAccel(0,4)
+
     listener()
